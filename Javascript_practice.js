b1491922496181cfbbee9b01436431f4a62138e4
abhishekyadav798 <99576491+abhishekyadav798@users.noreply.github.com>
2022-04-25 20:55:00 +0530

    Create Javascript_practice.js

---
+++ Javascript_practice.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Javascript_practice() {
+
+    var y = 11;
+    var x= 4;
+     var z = x +y;
+ console.log(z)
+   
+     
+  return (
+    <div>
+  
+    </div>
+  )
+}
+
+export default Javascript_practice
